0e190736cd5933fd5eda07dd686fcab6cda5cf3a
cti1587 <hi_tom@163.com>
2020-08-07 21:13:41 +0800

    Update command.

--- keys/test.py
+++ keys/test.py
@@ -1,19 +1,20 @@
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
-"https://www.baidu.com
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+"https://www.baidu.com"
+
+
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+
+
+import click
+
+
+click.echo()
--- test/hello.py
+++ test/hello.py
@@ -1,13 +1,9 @@
 #!/bin/env python
 
+
 def hello():
     print("111")
 
-
-    dsfdsaf
-
-
-    fuck
 
 def printDef():
     print("abc")
